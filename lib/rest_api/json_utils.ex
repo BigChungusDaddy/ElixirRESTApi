7670102cd88471185095395402fecf87f7835e35
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-07-19 10:40:26 -0600

    finished part3

---
+++ lib/rest_api/json_utils.ex
@@ -0,0 +1,23 @@
+defmodule RestApi.JSONUtils do
+  @moduledoc """
+  JSON Utilities
+  """
+
+  @doc """
+  Extend BSON to encode MongoDB ObjectIds to string
+  """
+  # Defining a implementation for the Jason.Encode for BSON.ObjectId
+  defimpl Jason.Encoder, for: BSON.ObjectId do
+    # Implementing a custom encode function
+    def encode(id, options) do
+      BSON.ObjectId.encode!(id)  # Converting the binary id to a string
+      |> Jason.Encoder.encode(options) # Encoding the string to JSON
+    end
+  end
+
+  def normaliseMongoId(doc) do
+    doc
+    |> Map.put('id', doc["_id"])
+    |> Map.delete("_id")
+  end
+end
